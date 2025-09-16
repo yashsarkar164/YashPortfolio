a8a4cffd6e3507439da919ba4207bf0ad71b313e
yashsarkar164 <132969742+yashsarkar164@users.noreply.github.com>
2025-09-16 18:50:21 +0000

    Deploying to gh-pages from @ yashsarkar164/YashPortfolio@b5a6493f7c0dcd93f49c569393a98e189396cc1b 🚀

---
+++ _next/static/QgDw1TCzhF1n7NL9A80FZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/b637e9a5-6cf11adc33c7fdd4.js","static/chunks/269-217414d22bda3f7c.js","static/chunks/pages/index-2b14b21ab257435d.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
