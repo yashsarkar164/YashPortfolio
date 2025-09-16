4535cad61a5fae1263abd3a24cdade527a3bd53a
YASH sarkar <132969742+yashsarkar164@users.noreply.github.com>
2025-09-16 23:45:26 +0530

    vscode changes

--- components/apps/vscode.js
+++ components/apps/vscode.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function VsCode() {
     return (
         <iframe
-            src="https://github1s.com/yashsarkar164/ImageHunt/blob/HEAD/script.js"
+            src="https://github1s.com/yashsarkar164/YashPortfolio/blob/main/components/apps/yash.js"
             frameBorder="0"
             title="VsCode"
             className="h-full w-full bg-ub-cool-grey">
